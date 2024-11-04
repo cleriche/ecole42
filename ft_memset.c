f878d34b7f9396ccf1540b05755086d2aa401c3b
cleriche <christophe.lrch@gmail.com>
2024-11-04 14:20:27 +0100

    Add files via upload
    libft temp

---
+++ ft_memset.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cleriche <cleriche@student.42nice.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/04 12:34:39 by cleriche          #+#    #+#             */
+/*   Updated: 2024/11/04 13:14:11 by cleriche         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	*ft_memset(void *ptr, int value, size_t n)
+{
+	size_t			i;
+	unsigned char	*p;
+
+	i = 0;
+	p = (unsigned char *)ptr;
+	while (i < n)
+	{
+		p[i] = (unsigned char)value;
+		i++;
+	}
+	return (ptr);
+}
